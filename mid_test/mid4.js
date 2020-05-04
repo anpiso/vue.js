26aeac3899729fb3fa3037e01cb6618c96b025f4
Anpiso <ehdwnd9238@gmail.com>
2020-05-04 10:00:55 +0900

    3차 commit

--- mid_test/mid4.js
+++ mid_test/mid4.js
@@ -1,23 +1,32 @@
-function sum(...arr){
-    let result = 0;
-    for(let i = 0; i < arr.length; ++i){
-        result += arr[i];
+let f = function sum(...arr){
+    let add = 0;
+    for(let i = 0; i < arr.length; i++){
+        add += arr[i];
     }
-    return result;
+    return add;
 }
 
-function test_sum(sum){
+const MAX = 10, MIN = 0;
+
+function test_sum(f){
     let arr = [];
-    const MAX = 10, MIN = 0;
+    let add = 0;
 
     for(let i = 0; i < 5; i++){
         arr[i] = Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
     }
-    let result = sum(arr);
+
+    console.log(arr);
+
+    let result = f(arr);
+
+    for(let i = 0; i < 5; i++){
+        add += arr[i];
+    }
     
-    console.log = result;
+    console.log(result);
+    console.log(add);
 }
 
-for(let i = 0; i < 5; i++){
-    console.log(test_sum);
-}
+for(let i = 0; i < 5; i++)
+    test_sum(f);
